fe2a669047d562a1d7eb89bebeaac326b42f2751
JeenatBhatia <bhatiajeenat08@gmail.com>
2025-09-25 01:34:55 +0530

    Save local changes before rebase

--- backend/server.js
+++ backend/server.js
@@ -33,5 +33,5 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
