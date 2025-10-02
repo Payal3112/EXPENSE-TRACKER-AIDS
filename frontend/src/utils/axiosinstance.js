9f954c448e237fc34d564b764dd695be1e4d2991
Payal3112 <k.payal3112@gmail.com>
2025-10-02 16:08:28 +0530

    add url in api paths and axios for deploying

--- frontend/src/utils/axiosinstance.js
+++ frontend/src/utils/axiosinstance.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { BASE_URL } from "./apiPaths";
 
-
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
   timeout: 10000,
@@ -9,6 +8,7 @@ const axiosInstance = axios.create({
     "Content-Type": "application/json",
     Accept: "application/json",
   },
+  withCredentials: true,
 });
 
 // Request Interceptor
@@ -20,16 +20,12 @@ axiosInstance.interceptors.request.use(
     }
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 // Response Interceptor
 axiosInstance.interceptors.response.use(
-  (response) => {
-    return response;
-  },
+  (response) => response,
   (error) => {
     if (error.response) {
       if (error.response.status === 401) {
